e8229761865c821a09d376fa51a536b7fb612c98
Di <tiandi619514@gmail.com>
2026-02-14 10:39:24 -0500

    hamiltonian start

---
+++ quantumgameoflife/hamiltonian.py
@@ -0,0 +1,10 @@
+from .cells import *
+def hamiltonian_boolean(cell_matrix):
+    '''
+    determine whether hamiltonian should be activated
+    return boolean matrix of the same size
+    '''
+    boolean_matrix = np.zeros(cell_matrix.shape, dtype = int)
+
+    return boolean_matrix
+
