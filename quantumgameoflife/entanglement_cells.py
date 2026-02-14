e41b07e04087f8435c96e436937c0eb037f9f828
Yurek Jankowski <yurek.jankowski@gmail.com>
2026-02-14 16:51:52 -0500

    finished implementing entagled version

--- quantumgameoflife/entanglement_cells.py
+++ quantumgameoflife/entanglement_cells.py
@@ -13,6 +13,8 @@ class Lattice:
 
         self.next_center_states = np.array([next_center_state(self.states[i]) for i in range(0, 512)])
 
+        self.steps_done = 0
+
         #grid in ford [x, y, an], with a coefficient of nth state
         self.initialize_grid(x, y, 0)
         self.alive_magnitudes = None
@@ -54,7 +56,8 @@ class Lattice:
         self.common_indices_neighbour = [self.common_indices_neighbour[i] for i in new_neighbour_ordering]
         self.common_indices_1 = [self.common_indices_1[i] for i in new_neighbour_ordering]
 
-
+        print(self.common_indices_1[3])
+        print(self.common_indices_neighbour[3])
 
         self.state_group_number = np.zeros((8, 512), dtype=int)
 
@@ -74,7 +77,7 @@ class Lattice:
             # state_neighbour = np.zeros((3, 3), dtype=bool)
 
             n_common_indices = len(common_indices_1)
-            for state_group_number in range(n_common_indices):
+            for state_group_number in range(2**n_common_indices):
                 self.states_alive[-1].append([])
                 self.states_dead[-1].append([])
                 b_array = nth_binary_array(state_group_number, n_common_indices)
@@ -85,33 +88,94 @@ class Lattice:
                     state = self.states[i]
                     if np.all(state[r_1, c_1] == b_array):
                         self.state_group_number[neighbour_number, i] = state_group_number
-                        for j in range(512):
-                            state_j = self.states[j]
-                            if np.all(state_j[r_neighbour, c_neighbour] == b_array):
-                                if self.next_center_states[j]:
-                                    self.states_alive[-1][-1].append(j)
-                                else:
-                                    self.states_dead[-1][-1].append(j)
+                for j in range(512):
+                    state_j = self.states[j]
+                    if np.all(state_j[r_neighbour, c_neighbour] == b_array):
+                        if self.next_center_states[j]:
+                            self.states_alive[-1][-1].append(j)
+                        else:
+                            self.states_dead[-1][-1].append(j)
+
+        # print(self.states[self.states_alive[3][self.state_group_number[3, 41]]][1])
+        # print(self.states[41])
+        #
+        # print(self.states_alive[0][self.state_group_number[0, 0]])
+        #
+
+        print(self.states[1])
+
+        # self.test()
+        # 1/0
+
+    def test(self):
+        current_coefficients = np.ones(512)
+        state_numbers = np.array([4])
+        # coefficient_sets = np.array([self.get_coefficients_for_state(pos, state_number) for state_number in state_numbers])
+        # coefficient_sets = np.array([[[0, 1], [1, 0], [1, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1]], [[0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0.5, 1]]])
+        # coefficient_sets = np.array([[1, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0.5, 1]])
+        coefficient_sets = np.array([[[0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1], [0, 1]]])
+        # coefficient_sets = np.array([[[0, 1], [0, 1], [0, 1], [1, 0], [1, 0], [1, 0], [1, 0], [1, 0]]])
+        # Assuming coefficient_sets has shape (n, 8, 2)
+        n = coefficient_sets.shape[0]
+
+        # Start with the first row for all n samples
+        r = coefficient_sets[:, -1, :].copy()  # shape (n, 2)
+
+        # Iteratively compute products
+        for i in range(6, -1, -1):
+            # print(i)
+            # r[:, :, np.newaxis] has shape (n, current_size, 1)
+            # a[:, i, np.newaxis, :] has shape (n, 1, 2)
+            # Broadcasting creates (n, current_size, 2), then flatten last two dims
+            r = (r[:, :, np.newaxis] * coefficient_sets[:, i, np.newaxis, :]).reshape(n, -1)
+            # r = np.concat(r[])
+
+        # r = r[:, ::-1]
+        alive_indices = self.next_center_states[state_numbers]
+        states_alive = r[alive_indices] * current_coefficients[state_numbers[alive_indices]][:, np.newaxis]
+        states_dead = r[~alive_indices] * current_coefficients[state_numbers[~alive_indices]][:, np.newaxis]
+
+        new_coefficient_sets_alive = np.sum(states_alive, axis=0)
+        new_coefficient_sets_dead = np.sum(states_dead, axis=0)
+
+        new_coefficients = np.concatenate([new_coefficient_sets_dead, new_coefficient_sets_alive])
+
+        st = np.where(new_coefficients)
+        print(st)
+        print(self.states[st[0]])
+
+    def run(self):
+        while 1:
+            self.step()
+            print(self.alive_magnitudes)
 
     def step(self):
         self.update_cells()
+        self.steps_done += 1
 
     def update_cells(self):
         pos = np.array([0, 0])
         all_state_numbers = np.arange(0, 512)
 
+        if self.steps_done == 1:
+            pass
+
         new_grid = np.zeros_like(self.grid)
         for x in range(self.x):
-            print(f"x: {x}")
+            # print(f"x: {x}")
             pos[1] = 0
             for y in range(self.y):
-                print(f"y: {y}")
+                if (x, y) == (2, 3):
+                    pass
+                # print(f"y: {y}")
                 if self.cutoff_for_states:
                     state_numbers = self.get_more_prominent_states(self.magnitudes[pos[0], pos[1]], self.cutoff_for_states)
                 else:
                     state_numbers = all_state_numbers
                 new_coefficients = self.get_new_cell_coefficients(pos, state_numbers, self.grid[pos[0], pos[1]])
                 new_grid[pos[0], pos[1]] = new_coefficients
+                pos[1] += 1
+            pos[0] += 1
         self.grid = new_grid
         self.normalize_grid_and_update_magnitudes()
 
@@ -124,7 +188,7 @@ class Lattice:
     def initialize_grid(self, x, y, configuration_number):
 
         if configuration_number == 0: #blinker
-            indices = [(4,3), (4, 4), (4, 5)]
+            indices = [(2,1), (2, 2), (2, 3)]
             self.grid = self.get_grid_for_single_state(x, y, indices)
         else:
             raise(NotImplementedError)
@@ -155,10 +219,7 @@ class Lattice:
         neighbour_pos = self.add_pos(pos, self.neighbour_offsets[neighbour_number])
         alive_coefficients = coefficient_grid[neighbour_pos[0], neighbour_pos[1], neighbour_states_alive]
         dead_coefficients = coefficient_grid[neighbour_pos[0], neighbour_pos[1], neighbour_states_dead]
-        return np.sum(alive_coefficients), np.sum(dead_coefficients)
-
-
-
+        return np.sum(dead_coefficients), np.sum(alive_coefficients)
 
     def get_coefficients_for_state(self, pos, state_number):
         coefficients = np.zeros((8, 2), dtype=np.complex128)
@@ -170,7 +231,7 @@ class Lattice:
         self.remove_0_states()
         magnitudes = np.abs(self.grid)**2
         cell_magnitudes = np.sum(magnitudes, axis=2)
-        print(cell_magnitudes.shape)
+        # print(cell_magnitudes.shape)
         self.grid = self.grid / cell_magnitudes[:, :, np.newaxis]**0.5
         self.magnitudes = np.abs(self.grid) ** 2
         self.alive_magnitudes = np.sum(self.magnitudes[:, :, 256::], axis=2)
@@ -186,7 +247,6 @@ class Lattice:
             s += sorted_magnitudes[i]
             if s >= cutoff:
                 return indices[:i+1]
-
         return indices
 
     def get_new_cell_coefficients(self, pos, state_numbers, current_coefficients):
@@ -197,17 +257,13 @@ class Lattice:
         # Assuming coefficient_sets has shape (n, 8, 2)
         n = coefficient_sets.shape[0]
 
-        # Start with the first row for all n samples
-        r = coefficient_sets[:, 0, :].copy()  # shape (n, 2)
+        r = coefficient_sets[:, -1, :].copy()  # shape (n, 2)
 
         # Iteratively compute products
-        for i in range(1, 8):
-            # r[:, :, np.newaxis] has shape (n, current_size, 1)
-            # coefficient_sets[:, i, np.newaxis, :] has shape (n, 1, 2)
-            # Broadcasting creates (n, current_size, 2), then flatten last two dims
+        for i in range(6, -1, -1):
             r = (r[:, :, np.newaxis] * coefficient_sets[:, i, np.newaxis, :]).reshape(n, -1)
+            # r = np.concat(r[])
 
-        r = r[:, ::-1]
         alive_indices = self.next_center_states[state_numbers]
         states_alive = r[alive_indices]*current_coefficients[state_numbers[alive_indices]][:, np.newaxis]
         states_dead = r[~alive_indices]*current_coefficients[state_numbers[~alive_indices]][:, np.newaxis]
@@ -275,10 +331,10 @@ def nth_binary_array(n, k):
     return ((n >> np.arange(k-1, -1, -1)) & 1).astype(int)
 
 
-l = Lattice(10, 10)
 
-for i in range(10):
-    l.update_cells()
+
+l = Lattice(5, 5)
+l.run()
 
 # l.update_cell_coefficients(np.array([1, 1]), [1])
 # states = create_states()
